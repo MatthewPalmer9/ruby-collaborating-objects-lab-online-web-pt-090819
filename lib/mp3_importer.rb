0c512cadca80e316bd87c24bf007c0ccf45b93e2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 18:21:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,7 @@ class MP3Importer
     Dir.entries(path).select{|entrie| entrie.include?(".mp3")}
   end
 
-  def import 
+  def import
     files.each(){|file| Song.new_by_filename(file)}
   end
 end
