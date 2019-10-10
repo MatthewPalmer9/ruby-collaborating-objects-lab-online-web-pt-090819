06540c1a59193f90e48b3175bfa1a75a6e9a8d27
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 18:31:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   def self.new_by_filename(filename)
     artist_obj, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist = artist_obj
+    new_song.artist_name = artist_obj
     new_song
   end
 
